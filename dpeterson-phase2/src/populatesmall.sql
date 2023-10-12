86db7f01a64afa72759672eae54307ec5890f6cc
dpatt27 <102685674+dpatt27@users.noreply.github.com>
2023-10-12 15:57:36 -0700

    Added to Candidates in populatesmall.sql

--- dpeterson-phase2/src/populatesmall.sql
+++ dpeterson-phase2/src/populatesmall.sql
@@ -4,6 +4,14 @@
 INSERT INTO Candidates(degree_level, age, years_experience, previous_occupation, race, religious_affiliation, phone_number, email, last_degree_issued, ssn, gender, background_check_data) VALUES
 ('PhD', 32, 8, 'Research Scientist', 'Asian', 'None', '1234567890', 'john_doe@email.com', 'PhD in Computer Science', 111223333, 'Male', 'Clear'),
 ('Masters', 28, 5, 'Data Analyst', 'Caucasian', 'Christian', '2345678901', 'jane_doe@email.com', 'MS in Data Science', 112233445, 'Female', 'Clear'),
+('Bachelors', 45, 13, 'Professor', 'Caucasian', 'Christian', '4678639083', 'jeff_white@email.com', 'BS in Data Science', 112233446, 'Male', 'Clear'),
+('PhD', 37, 12, 'Web Developer', 'African American', 'None', '8735658478', 'rebecca_light@email.com', 'PhD in Computer Science', 112233447, 'Female', 'Clear'),
+('Bachelors', 55, 28, 'Data Analyst', 'Caucasian', 'Christian', '0276472908', 'daniel_grip@email.com', 'BS in Data Science', 112233449, 'Female', 'Clear'),
+('Masters', 44, 20, 'Professor', 'Asian', 'Christian', '9835482618', 'clip_hamstring@email.com', 'MS in Computer Science', 112233442, 'Male', 'Clear'),
+('Bachelors', 32, 6, 'IT', 'Latino', 'None', '6373927643', 'jimi_mudstank@email.com', 'BS in Computer Science', 112233448, 'Male', 'Clear'),
+('None', 18, 0, 'Student', 'Caucasian', 'Christian', '9836554345', 'skippy_lip@email.com', 'GED', 112233476, 'Male', 'Clear'),
+('Masters', 66, 40, 'Research Scientist', 'None', 'Christian', '8735467321', 'lid_dip@email.com', 'MS in Data Science', 112233782, 'Male', 'Clear'),
+('Bachelors', 42, 15, 'Professor', 'Latino', 'Christian', '8735647625', 'stank_towel@email.com', 'BS in Data Science', 112233872, 'Female', 'Clear'),
 -- Add more tuples as needed...
 
 -- Populating Departments
