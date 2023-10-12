f66dbda943e29bbcf8c1fe996eb266df798f3ec7
Laurel Payne <143755268+laurellpayne@users.noreply.github.com>
2023-10-11 20:25:32 -0700

    Update dropschema.sql

---
+++ dropschema.sql
@@ -0,0 +1,8 @@
+-- dropschema.sql
+
+DROP TABLE IF EXISTS Rejection_Letters;
+DROP TABLE IF EXISTS Offer_Letters;
+DROP TABLE IF EXISTS Applications_Received;
+DROP TABLE IF EXISTS Jobs;
+DROP TABLE IF EXISTS Candidates;
+DROP TABLE IF EXISTS Departments;
