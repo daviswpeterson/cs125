93ce20bc69ab67866f1437646a4485bae07b8dab
Laurel Payne <143755268+laurellpayne@users.noreply.github.com>
2023-10-12 15:27:14 -0700

    Updated to match create schema dropschema.sql

--- dpeterson-phase2/src/dropschema.sql
+++ dpeterson-phase2/src/dropschema.sql
@@ -1,8 +1,7 @@
 -- dropschema.sql
 
-DROP TABLE IF EXISTS Rejection_Letters;
-DROP TABLE IF EXISTS Offer_Letters;
-DROP TABLE IF EXISTS Applications_Received;
+DROP TABLE IF EXISTS Letters;
+DROP TABLE IF EXISTS Applications;
 DROP TABLE IF EXISTS Jobs;
 DROP TABLE IF EXISTS Candidates;
 DROP TABLE IF EXISTS Departments;
