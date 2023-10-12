2876ea53f15db7fce6eabca7fe7c920b229cecd8
Laurel Payne <143755268+laurellpayne@users.noreply.github.com>
2023-10-12 16:23:59 -0700

    Update populatelarge.sql

--- dpeterson-phase2/src/populatelarge.sql
+++ dpeterson-phase2/src/populatelarge.sql
@@ -1,4 +1,6 @@
--- populatelarge.sql
-
-COPY Candidates FROM '/Users/laurelpayne/Desktop/candidates_large.csv' DELIMITER ',' CSV HEADER;
-
+-- For PostgreSQL
+COPY Candidates FROM '/Users/laurelpayne/Desktop/Gen/candidates.csv' DELIMITER ',' CSV HEADER;
+COPY Applications FROM '/Users/laurelpayne/Desktop/Gen/applications.csv' DELIMITER ',' CSV HEADER;
+COPY Departments FROM '/Users/laurelpayne/Desktop/Gen/departments.csv' DELIMITER ',' CSV HEADER;
+COPY Jobs FROM '/Users/laurelpayne/Desktop/Gen/jobs.csv' DELIMITER ',' CSV HEADER;
+COPY Letters FROM '/Users/laurelpayne/Desktop/Gen/letters.csv' DELIMITER ',' CSV HEADER;
