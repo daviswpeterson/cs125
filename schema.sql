068114db084a32585b4b26f6ae8baa47d6a2989c
Laurel Payne <143755268+laurellpayne@users.noreply.github.com>
2023-10-11 20:10:27 -0700

    Schema sql viscose

---
+++ schema.sql
@@ -0,0 +1,69 @@
+CREATE TABLE Candidates (
+    degree_level VARCHAR(255),
+    age INT,
+    years_experience INT,
+    previous_occupation VARCHAR(255),
+    race VARCHAR(255),
+    religious_affiliation VARCHAR(255),
+    phone_number INT,
+    email_address VARCHAR(255),
+    last_degree_issued VARCHAR(255),
+    ssn INT NOT NULL,
+    gender VARCHAR(255),
+    background_check_data VARCHAR(255),
+    PRIMARY KEY(ssn)
+);
+
+CREATE TABLE Jobs (
+    job_id INT NOT NULL,
+    position_title VARCHAR(255),
+    department VARCHAR(255),
+    tenure BOOLEAN,
+    full_time BOOLEAN,
+    part_time BOOLEAN,
+    adjunct BOOLEAN,
+    alumnus BOOLEAN,
+    hourly BOOLEAN,
+    per_hour_wage FLOAT,
+    salary INT,
+    listing_date VARCHAR(255),
+    end_listing_date VARCHAR(255),
+    status_of_position VARCHAR(255),
+    opening BOOLEAN,
+    PRIMARY KEY (job_id)
+);
+
+CREATE TABLE Applications_Received (
+    application_id INT NOT NULL,
+    department VARCHAR(255),
+    position VARCHAR(255),
+    tenure_track BOOLEAN,
+    date DATE,
+    person VARCHAR(255),  -- Assuming the 'person' attribute is a VARCHAR for now. You may want to reference the Candidates table if this refers to a specific candidate.
+    PRIMARY KEY (application_id)
+);
+
+CREATE TABLE Offer_Letters (
+    offer_id INT NOT NULL,
+    job_title VARCHAR(255),
+    salary INT,
+    start_date DATE,
+    next_steps VARCHAR(255),
+    PRIMARY KEY (offer_id)
+);
+
+CREATE TABLE Rejection_Letters (
+    offer_id INT NOT NULL,
+    person VARCHAR(255),
+    rejection_date DATE,
+    denial_reason VARCHAR(255),
+    PRIMARY KEY (offer_id)
+);
+
+CREATE TABLE Departments (
+    name VARCHAR(255) NOT NULL,
+    abbreviation VARCHAR(255),
+    max_faculty_capacity INT,
+    current_number_of_faculty INT,
+    PRIMARY KEY (name)
+);
