6faf566dda6ecddc723f1c15b6fa4843860eba53
Laurel Payne <143755268+laurellpayne@users.noreply.github.com>
2023-10-12 20:16:02 -0700

    Update dropschema.sql

--- dpeterson-phase2/src/dropschema.sql
+++ dpeterson-phase2/src/dropschema.sql
@@ -1,7 +1,7 @@
 -- dropschema.sql
 
+DROP TABLE IF EXISTS Departments;
 DROP TABLE IF EXISTS Letters;
 DROP TABLE IF EXISTS Applications;
 DROP TABLE IF EXISTS Jobs;
 DROP TABLE IF EXISTS Candidates;
-DROP TABLE IF EXISTS Departments;
