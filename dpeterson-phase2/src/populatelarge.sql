024d44b2a44814f174893a704bf5d2bb1538a13f
ConnorRogstad <143755355+ConnorRogstad@users.noreply.github.com>
2023-10-12 20:53:08 -0700

    Update populatelarge.sql

--- dpeterson-phase2/src/populatelarge.sql
+++ dpeterson-phase2/src/populatelarge.sql
@@ -1,33 +1,33 @@
 -- For MariaDB (MySQL)
-LOAD DATA INFILE 'https://github.com/daviswpeterson/cs125/blob/main/dpeterson-phase2/src/candidates.csv'
+LOAD DATA INFILE '/Desktop/cs125/crogstad-phase2/candidates.csv'
 INTO TABLE Candidates
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-LOAD DATA INFILE 'https://github.com/daviswpeterson/cs125/blob/main/dpeterson-phase2/src/applications.csv'
+LOAD DATA INFILE '/Desktop/cs125/crogstad-phase2/applications.csv'
 INTO TABLE Applications
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-LOAD DATA INFILE 'https://github.com/daviswpeterson/cs125/blob/main/dpeterson-phase2/src/departments.csv'
+LOAD DATA INFILE '/Desktop/cs125/crogstad-phase2/departments.csv'
 INTO TABLE Departments
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-LOAD DATA INFILE 'https://github.com/daviswpeterson/cs125/blob/main/dpeterson-phase2/src/jobs.csv'
+LOAD DATA INFILE '/Desktop/cs125/crogstad-phase2/jobs.csv'
 INTO TABLE Jobs
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-LOAD DATA INFILE 'https://github.com/daviswpeterson/cs125/blob/main/dpeterson-phase2/src/letters.csv'
+LOAD DATA INFILE '/Desktop/cs125/crogstad-phase2/letters.csv'
 INTO TABLE Letters
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
