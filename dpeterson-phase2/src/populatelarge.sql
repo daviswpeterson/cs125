1966e065da693b9ba008be2a3ac69849f11ab0e1
Lrozinskas <102685657+Lrozinskas@users.noreply.github.com>
2023-10-19 16:44:41 -0700

    Updated  populatelarge.sql for LOCAL stuff

--- dpeterson-phase2/src/populatelarge.sql
+++ dpeterson-phase2/src/populatelarge.sql
@@ -1,33 +1,33 @@
 -- For MariaDB (MySQL)
-LOAD DATA INFILE '/Desktop/cs125/crogstad-phase2/candidates.csv'
+LOAD DATA LOCAL INFILE '/Desktop/cs125/crogstad-phase2/candidates.csv'
 INTO TABLE Candidates
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-LOAD DATA INFILE '/Desktop/cs125/crogstad-phase2/applications.csv'
+LOAD DATA LOCAL INFILE '/Desktop/cs125/crogstad-phase2/applications.csv'
 INTO TABLE Applications
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-LOAD DATA INFILE '/Desktop/cs125/crogstad-phase2/departments.csv'
+LOAD DATA LOCAL INFILE '/Desktop/cs125/crogstad-phase2/departments.csv'
 INTO TABLE Departments
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-LOAD DATA INFILE '/Desktop/cs125/crogstad-phase2/jobs.csv'
+LOAD DATA LOCAL INFILE '/Desktop/cs125/crogstad-phase2/jobs.csv'
 INTO TABLE Jobs
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-LOAD DATA INFILE '/Desktop/cs125/crogstad-phase2/letters.csv'
+LOAD DATA LOCAL INFILE '/Desktop/cs125/crogstad-phase2/letters.csv'
 INTO TABLE Letters
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
