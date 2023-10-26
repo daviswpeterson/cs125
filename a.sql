02d0fb8abd34aab38f9abb346354b39f125fcf5f
daviswpeterson <143755072+daviswpeterson@users.noreply.github.com>
2023-10-26 16:42:45 -0700

    Created a.sql, added some skeleton code

---
+++ a.sql
@@ -0,0 +1,25 @@
+
+/*
+Question 1:
+*/
+SELECT * FROM *
+  
+/*
+Question 2:
+*/
+SELECT * FROM *
+  
+/*
+Question 3:
+*/
+SELECT * FROM *
+  
+/*
+Question 4:
+*/
+SELECT * FROM *
+  
+/*
+Question 5:
+*/
+SELECT * FROM *
