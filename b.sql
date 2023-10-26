ae45bf039a2f7bb1c7f98056339e31f1144f2c0f
daviswpeterson <143755072+daviswpeterson@users.noreply.github.com>
2023-10-26 16:45:06 -0700

    Created b.sql and added skeleton code

---
+++ b.sql
@@ -0,0 +1,25 @@
+
+/*
+Goal 1: 
+*/
+SELECT * FROM *
+
+/*
+Goal 2: 
+*/
+SELECT * FROM *
+
+/*
+Goal 3: 
+*/
+SELECT * FROM *
+
+/*
+Goal 4: 
+*/
+SELECT * FROM *
+
+/*
+Goal 5: 
+*/
+SELECT * FROM *
