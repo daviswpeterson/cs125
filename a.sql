e206ca894757bd5ca18700fda82e3d887d3d6221
darianchoi <143754775+darianchoi@users.noreply.github.com>
2023-10-30 21:11:52 -0700

    Final a.SQL

--- a.sql
+++ a.sql
@@ -2,34 +2,38 @@
 /*
 Question 1: Retrieve the details of candidates who have applied for an Associate Professor position along with their contact information.
 */
-SELECT Candidates.*, Applications.Date
+SELECT Candidates.*, Applications.date
 FROM Candidates 
-INNER JOIN Applications ON Candidates.Email = Applications.Candidate
-WHERE Applications.Job = 'Associate Professor';
-  
+INNER JOIN Applications ON Candidates.email = Applications.Candidate
+WHERE Applications.job = 'Associate Professor';
+
 /*
 Question 2: Find the average salary for all the jobs listed in the database.
 */
-SELECT AVG(Salary) AS AverageSalary
+SELECT AVG(salary) AS AverageSalary
 FROM Jobs;
+
   
 /*
 Question 3: Find the department with the maximum number of faculty capacity.
 */
-SELECT Name, Max_Faculty_Capacity
+SELECT name, max_faculty_capacity
 FROM Departments
-WHERE Max_Faculty_Capacity = (SELECT MAX(Max_Faculty_Capacity) FROM Departments);
+WHERE max_faculty_capacity = (SELECT MAX(max_faculty_capacity) FROM Departments);
+
+
   
 /*
 Question 4: Retrieve the details of candidates who have a PhD and more than 10 years of experience.
 */
 SELECT *
 FROM Candidates
-WHERE Degree_Level = 'PhD' AND Years_of_Experience > 10;
-  
+WHERE degree_level = 'PhD' AND years_experience > 10;
+
 /*
 Question 5: Find the job listings that have not yet ended.
 */
 SELECT *
 FROM Jobs
-WHERE End_Listing_Date > GETDATE();
+WHERE end_listing_date > GETDATE();
+
