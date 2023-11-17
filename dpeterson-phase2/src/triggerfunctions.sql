bf2787195aa7646a56a83f9422ff85073c03ca3d
daviswpeterson <143755072+daviswpeterson@users.noreply.github.com>
2023-11-16 16:57:19 -0800

    Updated triggerfunctions.sql

--- dpeterson-phase2/src/triggerfunctions.sql
+++ dpeterson-phase2/src/triggerfunctions.sql
@@ -1,7 +1,16 @@
-CREATE FUNCTION sample3.function_name ()
-RETURNS DECIMAL(6,5) AS
+CREATE PROCEDURE 
+    procedure_name
+    MODIFIES SQL DATA
 BEGIN
-    ratio = SELECT MAX(years_experience / age) FROM Candidates;
-    RETURN ratio
-END;
-
+    SET Counter=1
+        WHILE (Counter < SELECT Count(*) FROM Jobs)
+        /*
+        Iterate through the rows of Jobs
+        For each row SELECT the number of openings for each departments
+        SET (for Departments) current_number_of_faculty = max_faculty_capacity - the above value 
+        */
+        BEGIN
+            UPDATE Departments
+        END
+    
+        
