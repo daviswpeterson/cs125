66cb39a04a5fa501f107a377cbfcbb34c159dcdc
daviswpeterson <143755072+daviswpeterson@users.noreply.github.com>
2023-11-16 19:32:55 -0800

    Updated triggerfunctions.sql

--- dpeterson-phase2/src/triggerfunctions.sql
+++ dpeterson-phase2/src/triggerfunctions.sql
@@ -5,7 +5,6 @@ CREATE PROCEDURE
 BEGIN
     DECLARE jobs_cursor CURSOR FOR SELECT department, openings FROM Jobs;
     OPEN jobs_cursor;
-    FETCH NEXT FROM jobs_cursor;
         WHILE (@@FETCH_STATUS = 0)
         /*
         max faculty - current faculty - openings
@@ -14,7 +13,7 @@ BEGIN
             FETCH NEXT FROM jobs_cursor INTO dept, number_of_openings;
             UPDATE Departments
             SET unutilized_positions = Departments.max_faculty_capacity - Departments.current_number_of_faculty - number_of_openings
-            WHERE Departments.name = dept;
+            WHERE Departments.name = jobs_cursor.dept;
         END;
     CLOSE jobs_cursor;
     DEALLOCATE jobs_cursor;
